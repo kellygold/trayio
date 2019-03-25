b9b9299d1be774c715fa4333638a69139f7344ac
kellygold <kellytgold@gmail.com>
2019-03-24 19:31:20 -0600

    Fixed counting dirt spot twice

--- hoover.js
+++ hoover.js
@@ -3,35 +3,33 @@
 // We don't care about how efficent the robot is moving because moving instructions are in the program input
 // Let's get started
 
-const fs = require('fs');
+//** After numerous checks and close debugging, the ouput of the example online looks to be 1 off, I believe the output should be 2*/
 
-// read input file
-// I am assuming that my input is clean always
+const fs = require('fs');
+var _ = require('lodash');
+//read input file
 input = fs.readFileSync("input.txt").toString()
 
 //parse file and define initial conditions
-inputArr = input.split('\n')
-let hooverPaths = inputArr.pop().split('')
+inputArr = input.split('\n');
+let hooverPaths = inputArr.pop().split('');
 let jsonCoordinates = inputArr.map(line => {
     lineArr = line.split(' ');
     jsonCoords = {X: lineArr[0], Y: lineArr[1]}
-    return jsonCoords
+    return jsonCoords;
 })
 
-
+//define variables / parse the input data
 let dimensions = jsonCoordinates.shift();
 let startPosition = jsonCoordinates.shift();
-let dirtSpots = jsonCoordinates
-let finalPosition = startPosition
-let dirtCleanedCount = 0
-// console.log(dimensions)
-// console.log(startPosition)
-// console.log(dirtSpots)
-// console.log(hooverPaths)
+let dirtSpots = jsonCoordinates;
+let finalPosition = startPosition;
+let dirtCleanedCount = 0;
+let cleanedSpots = []
 
 hooverPaths.map(direction => {
-    // Make sure we didn't hit the edge, otherwise skip to the next move
-    if (finalPosition.X < dimensions.X && finalPosition.Y < dimensions.Y && (finalPosition.X > 0) && (finalPosition.Y > 0)){
+    // Make sure we won't hit the edge, otherwise skip to the next move
+    if ( (finalPosition.X < dimensions.X) && (finalPosition.Y < dimensions.Y) && (finalPosition.X > 0) && (finalPosition.Y > 0) ){
         //increment the coordinates based on cardinal direction
         if (direction === "N") {
             finalPosition.Y++
@@ -52,12 +50,15 @@ hooverPaths.map(direction => {
         dirtSpots.map(spotCoords => {
             if (spotCoords.X == finalPosition.X && spotCoords.Y == finalPosition.Y) {
                 dirtCleanedCount++
+                cleanedSpots.push(spotCoords)
             }
         })
-
 }
 })
 
-//output the results, clean up the output from the JSON coords
+// Make sure you only count a cleaned spot once.
+uniqueSpotsCleaned = _.sortedUniq(cleanedSpots).length
+
+// output the results, clean up the output from the JSON coords
 console.log(finalPosition.X + ' ' + finalPosition.Y)
-console.log(dirtCleanedCount)
+console.log(uniqueSpotsCleaned)
